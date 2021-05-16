2d7075c15dc700a4cdecdf11cc92f8d6321f2eca
DESKTOP-2KIND8H\Raphael Tuksa <rapha.tuksa@hotmail.com>
2021-05-16 17:16:08 +0200

    Test1

--- src/gad/dynamicarray/DynamicArray.java
+++ src/gad/dynamicarray/DynamicArray.java
@@ -4,8 +4,20 @@ import java.util.Arrays;
 
 public class DynamicArray {
     private int[] elements;
+    private int currentSize;
+    private int growthFactor;
+    int maxOverhead;
 
     public DynamicArray(int growthFactor, int maxOverhead) {
+
+        if(growthFactor < 2 || maxOverhead < 1){
+            throw new IllegalArgumentException();
+        } else{
+            currentSize = 4;
+            elements = new int[currentSize];
+            this.growthFactor = growthFactor;
+            this.maxOverhead = maxOverhead;
+        }
     }
 
     public int getLength() {
@@ -13,14 +25,27 @@ public class DynamicArray {
     }
 
     public Interval reportUsage(Interval usage, int minSize) {
-        return null;
+        if(minSize > currentSize){
+            int secondArray[] = new int[currentSize * growthFactor];
+            for (int i = 0; i < currentSize; i++) {
+                secondArray[i] = elements[i];
+            }
+            elements = secondArray;
+        } else if(minSize < currentSize){
+            int secondArray[] = new int[currentSize / growthFactor];
+            for (int i = 0; i < secondArray.length; i++) {
+                secondArray[i] = elements[i];
+            }
+        }
+        return usage;
     }
 
     public int get(int index) {
-        return 0;
+        return elements[index];
     }
 
     public void set(int index, int value) {
+        elements[index] = value;
     }
 
     public void reportArray(Result result) {
